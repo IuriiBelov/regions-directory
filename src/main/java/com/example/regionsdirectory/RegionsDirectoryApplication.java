0168a6787267ae657342848244a6fbb62e1470c4
Iurii Belov <iurii.belob.93@mail.ru>
2021-09-08 13:07:02 +0300

    Created empty project

---
+++ src/main/java/com/example/regionsdirectory/RegionsDirectoryApplication.java
@@ -0,0 +1,14 @@
+package com.example.regionsdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionsDirectoryApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RegionsDirectoryApplication.class, args);
+	}
+
+}
