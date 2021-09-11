7e9b0538f1378e9794ce5289cecf9670f1f195c8
Iurii Belov <iurii.belob.93@mail.ru>
2021-09-11 19:10:39 +0300

    Added Swagger.

--- src/main/java/com/example/regionsdirectory/controller/RegionController.java
+++ src/main/java/com/example/regionsdirectory/controller/RegionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("regions")
 public class RegionController {
 
     private final RegionService regionService;
